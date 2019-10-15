738d877c234652b006ebba099bfd9bf4c7198bfe
Illia Levandovskyi <lis2889@gmail.com>
2019-10-16 00:39:57 +0300

    tutorial 5 - use exchange with type topic

---
+++ EmitLogTopic/EmitLogTopicProgram.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using RabbitMQ.Client;
+
+namespace EmitLogTopic
+{
+    class EmitLogTopicProgram
+    {
+        private const string ExchangeName = "logs_topic";
+        static void Main(string[] args)
+        {
+            var connectionFactory = new ConnectionFactory() { HostName = "localhost" };
+            using (var connection = connectionFactory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                channel.ExchangeDeclare(ExchangeName, ExchangeType.Topic);
+
+                (string message, string facility, string severity) = GetMessage(args);
+                var body = Encoding.UTF8.GetBytes(message);
+
+                channel.BasicPublish(ExchangeName, $"{facility}.{severity}", body: body);
+                Console.WriteLine($"Sent {facility}.{severity}: {message}");
+            }
+
+            Console.WriteLine("to exit the application press enter");
+            Console.ReadLine();
+        }
+
+        private static (string, string, string) GetMessage(string[] args)
+        {
+            if (args == null || args.Length == 0)
+                return ("incubator", "anonymous", "info");
+            
+            var keyParts = args[0].Split(new[] {'.'}, StringSplitOptions.RemoveEmptyEntries);
+            var message = (args.Length == 1)
+                ? "obfuscator"
+                : string.Join(" ", args.Skip(1));
+            
+            return (message, keyParts.First(), keyParts.Last());
+        }
+    }
+}
---
+++ ReceiveLogsTopic/ReceiveLogsTopicProgram.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+namespace ReceiveLogsTopic
+{
+    class ReceiveLogsTopicProgram
+    {
+        private const string ExchangeName = "logs_topic";
+
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory() {HostName = "localhost"};
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                channel.ExchangeDeclare(ExchangeName, ExchangeType.Topic);
+                var queueName = channel.QueueDeclare().QueueName;
+
+                if (!CreateBindings(channel, ExchangeName, queueName, args))
+                {
+                    Environment.ExitCode = 1;
+                    return;
+                }
+
+                var consumer = new EventingBasicConsumer(channel);
+                consumer.Received += OnMessageDelivered;
+
+                channel.BasicConsume(queueName, true, consumer);
+                Console.WriteLine("To exit the application press enter");
+                Console.ReadLine();
+            }
+        }
+
+        private static bool CreateBindings(IModel channel, string exchangeName, string queueName, string[] args)
+        {
+            if (args == null || args.Length == 0)
+            {
+                Console.WriteLine("Usage is [binding_key...]");
+                Console.WriteLine("To exit the application press enter");
+                Console.ReadLine();
+                return false;
+            }
+
+            foreach (var routingKey in args)
+            {
+                channel.QueueBind(queueName, exchangeName, routingKey);
+            }
+
+            return true;
+        }
+
+        private static void OnMessageDelivered(object sender, BasicDeliverEventArgs e)
+        {
+            var message = Encoding.UTF8.GetString(e.Body);
+            Console.WriteLine($"Received {e.RoutingKey}: {message}");
+        }
+    }
+}
